a5ba70011f765841c15649a3cc61ba31a0b97024
jtobelem <jtobelem@simplon.co>
2021-10-08 08:04:20 +0000

    Saved recipe: PLANETEOUI.compute_ytrain_long

--- recipes/compute_ytrain_long.py
+++ recipes/compute_ytrain_long.py
@@ -16,7 +16,7 @@ ytrain_long_df = ytrain_NpxebDC_df # For this sample code, simply copy input to 
 
 
 
-pd.melt(ytrain_NpxebDC_df, id_vars=['timestamp'], value_vars=list(ytrain_long_df.columns[1:]), var_name='station', value_name='status')
+ytrain_long_df = pd.melt(ytrain_NpxebDC_df, id_vars=['timestamp'], var_name='station', value_name='status')
 
 
 # Write recipe outputs
