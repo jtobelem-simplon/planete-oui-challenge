1063e4321efcb2a844a217035e41ba7f933e09ed
jtobelem <jtobelem@simplon.co>
2021-10-12 12:06:28 +0000

    Saved recipe: PLANETEOUI.compute_forecast_final

---
+++ recipes/compute_forecast_final.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+import dataiku
+import pandas as pd, numpy as np
+from dataiku import pandasutils as pdu
+
+# Read recipe inputs
+forecast_prepared = dataiku.Dataset("forecast_prepared")
+forecast_prepared_df = forecast_prepared.get_dataframe()
+
+
+# Compute recipe outputs from inputs
+# TODO: Replace this part by your actual code that computes the output, as a Pandas dataframe
+# NB: DSS also supports other kinds of APIs for reading and writing data. Please see doc.
+
+forecast_final_df = forecast_prepared_df.pivot_table(values='status', index='timestamp', columns='station', aggfunc='first').reset_index()
+
+
+# Write recipe outputs
+forecast_final = dataiku.Dataset("forecast_final")
+forecast_final.write_with_schema(forecast_final_df)
