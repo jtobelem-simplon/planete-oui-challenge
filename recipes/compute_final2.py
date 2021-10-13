3e115b161ac4b3ce928eea91e56df3cdb44126c8
jtobelem <jtobelem@simplon.co>
2021-10-13 13:56:41 +0000

    Saved recipe: PLANETEOUI.compute_final2

---
+++ recipes/compute_final2.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+import dataiku
+import pandas as pd, numpy as np
+from dataiku import pandasutils as pdu
+
+# Read recipe inputs
+ytest_joined_prepared_scored = dataiku.Dataset("ytest_joined_prepared_scored")
+ytest_joined_prepared_scored_df = ytest_joined_prepared_scored.get_dataframe()
+
+
+# Compute recipe outputs from inputs
+# TODO: Replace this part by your actual code that computes the output, as a Pandas dataframe
+# NB: DSS also supports other kinds of APIs for reading and writing data. Please see doc.
+
+final2_df = ytest_joined_prepared_scored_df.pivot_table(values='status', index='timestamp', columns='station', aggfunc='first').reset_index()
+
+
+# Write recipe outputs
+final2 = dataiku.Dataset("final2")
+final2.write_with_schema(final2_df)
